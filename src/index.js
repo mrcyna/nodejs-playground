f240dacfa8578e1845f760dddd41a619db1f41bd
Sina Ahmadpour <sina.ahmadpour@otrium.com>
2022-12-27 22:45:12 +0100

    remove mistakenly built file

--- src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Greeting_1 = require("./Greeting");
-console.log(Greeting_1.Greeting.sayHello('World!'));
--- src/module/greeting.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Greeting = void 0;
-class Greeting {
-    static sayHello(name) {
-        return `Hello ${name}`;
-    }
-}
-exports.Greeting = Greeting;
